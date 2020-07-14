b3c73961f6aa203dcd599d49be7a768f5fdd9353
barteks2236 <barteks2236@wp.pl>
2020-07-14 22:54:12 +0200

    stworzenie podstawowych klas

--- src/main/java/com/Saper/ver1_0/BombCounter.java
+++ src/main/java/com/Saper/ver1_0/BombCounter.java
@@ -2,4 +2,11 @@ package com.Saper.ver1_0;
 
 public class BombCounter {
 
+	private int bombCounter(int x, int y) {
+		
+		
+	
+		
+	return 0;	
+	}
 }
--- src/main/java/com/Saper/ver1_0/GamePanel.java
+++ src/main/java/com/Saper/ver1_0/GamePanel.java
@@ -1,15 +1,40 @@
 package com.Saper.ver1_0;
 
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
-	
+
 	public GamePanel() {
 
+		setLayout(new GridLayout(2,6));
+
+		JButton bombButton = new JButton();
+		bombButton.setBounds(100, 100, 50, 50);
+		for (int i=0 ; i<2 ; i++) {
+			for (int j=0 ; j<2 ; j++) {
+				add(new JButton(""+ (j+1)));
+			}
+		}
+		setVisible(true);
+//		bombButton.addActionListener(new ActionListener() {
+//
+////			@Override
+//			public void actionPerformed(ActionEvent e) {
+//
+//			}
+//
+//		});
+
+
+
 	}
-	
+
 //  Override
 	public Dimension getPreferredSize() {
 		return new Dimension(800, 800);
