02df3b76d797b48e28a2a8c1602439e7af18a5e5
barteks2236 <barteks2236@wp.pl>
2020-07-14 21:34:02 +0200

    stworzenie projektu

--- src/main/java/com/Saper/ver1_0/GamePanel.java
+++ src/main/java/com/Saper/ver1_0/GamePanel.java
@@ -1,5 +1,17 @@
 package com.Saper.ver1_0;
 
-public class GamePanel {
+import java.awt.Dimension;
 
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+	
+	public GamePanel() {
+
+	}
+	
+//  Override
+	public Dimension getPreferredSize() {
+		return new Dimension(800, 800);
+	}
 }
--- src/main/java/com/Saper/ver1_0/Main.java
+++ src/main/java/com/Saper/ver1_0/Main.java
@@ -1,9 +1,22 @@
 package com.Saper.ver1_0;
 
+import javax.swing.JFrame;
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+		JFrame gameArea = new JFrame("Saper/Mines");
+		GamePanel gamePanel = new GamePanel();
+
+		gameArea.add(gamePanel);
+		gameArea.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameArea.setVisible(true);
+		gameArea.pack();
+	
+
 
 	}
 
